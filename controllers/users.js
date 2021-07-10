80056126bf680717db7019013480bd256c6bbded
Nikolay Astakhayev <astakhayev@yandex.ru>
2021-07-10 22:32:22 +0800

    Добавить контроллеры пользователя
    Контроллеры: создание пользователя, аутентификация, получение пользователя и изменение.

---
+++ controllers/users.js
@@ -0,0 +1,99 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+const ValidationError = require('../errors/valid-err');
+const AuthError = require('../errors/auth-err');
+const NotFoundError = require('../errors/not-found-err');
+const ConflictError = require('../errors/conflict-err');
+
+module.exports.getMe = (req, res, next) => {
+  User.findById(req.user._id)
+    .orFail()
+    .select('name email')
+    .then((user) => {
+      if (!user) {
+        throw new AuthError('Необходима авторизация.');
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'DocumentNotFoundError') {
+        throw new NotFoundError('Пользователь по указанному _id не найден.');
+      } else if (err.name === 'CastError') {
+        throw new ValidationError('Некорректный _id.');
+      } else {
+        throw err;
+      }
+    })
+    .catch(next);
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name, about, avatar, email,
+  } = req.body;
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({
+      name, email, password: hash,
+    }))
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new ValidationError('Переданы некорректные данные при создании пользователя.');
+      } else if (err.name === 'MongoError' && err.code === 11000) {
+        throw new ConflictError('Вы уже регистрировались.');
+      } else {
+        throw err;
+      }
+    })
+    .catch(next);
+};
+
+const { JWT_SECRET = '0de50296aeea456249151bd8278d04515e1f6a8d490db398ad285b0c3eec9676' } = process.env;
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id },
+        JWT_SECRET,
+        { expiresIn: '7d' });
+      res.send({ token, data: user })
+        .end();
+    })
+    .catch(next);
+};
+
+module.exports.updateUserInfo = (req, res, next) => {
+  const { name, email } = req.body;
+  if (!name || !email) {
+    throw new ValidationError('Переданы некорректные данные при обновлении пользователя.');
+  }
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, email },
+    { new: true, runValidators: true },
+  )
+    .orFail()
+    .select('name email')
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь с указанным _id не найден.');
+      }
+      res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        throw new ValidationError('Переданы некорректные данные при обновлении пользователя.');
+      } else if (err.name === 'DocumentNotFoundError') {
+        throw new NotFoundError('Пользователь по указанному _id не найден.');
+      } else if (err.name === 'CastError') {
+        throw new ValidationError('Некорректный _id.');
+      } else {
+        throw err;
+      }
+    })
+    .catch(next);
+};
